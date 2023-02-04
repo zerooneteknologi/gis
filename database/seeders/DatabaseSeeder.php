6d3af15a52e2417ea081e44f2eaf5d3552d6a35b
sasanurjaman <nurjamansasa97@gmail.com>
2023-02-04 12:31:00 +0700

    auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Str;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +19,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,49 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+@include('partials.meta')
+<!-- [ auth-signin ] start -->
+<div class="auth-wrapper">
+	<div class="auth-content">
+		<div class="card">
+			<div class="row align-items-center text-center">
+				<div class="col-md-12">
+                    <form action="{{ route('login')}}">
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
+						<div class="card-body">
+							<img src="assets/images/logo-dark.svg" alt="" class="img-fluid mb-4">
+							<h4 class="mb-3 f-w-400">Signin</h4>
+							<div class="input-group mb-3">
+								<span class="input-group-text"><i data-feather="mail"></i></span>
+								<input name="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email address" required autocomplete="email">
+								@error('email')
+								<div class="invalid-feedback">
+									<strong>{{ $message }}</strong>
+								</div>
+								@enderror
+							</div>
+							<div class="input-group mb-4">
+								<span class="input-group-text"><i data-feather="lock"></i></span>
+								<input name="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" required>
+								@error('password')
+								<div class="invalid-feedback">
+									<strong>{{ $message }}</strong>
+								</div>
+								@enderror
+							</div>
+							<div class="form-group text-left mt-2">
+								<div class="form-check">
+									<input name="remember" class="form-check-input" type="checkbox" value="{{ old('remember') ? 'checked' : '' }}" id="flexCheckChecked" checked>
+									<label class="form-check-label" for="flexCheckChecked">
+										Remember me
+									</label>
+								</div>
+							</div>
+							<button class="btn btn-block btn-primary mb-4" type="submit">Signin</button>
+						</div>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-@endsection
+<!-- [ auth-signin ] end -->
+@include('partials.footer')
